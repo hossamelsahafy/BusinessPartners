8e4478b9f9ae7ce88b428600b54bdfff9a486c3e
hossamelsahafy <hossamelsa7afy54@gmail.com>
2025-09-14 11:49:47 +0300

    Fix Responsive

--- src/app/(frontend)/[locale]/_components/Home/Header/SearchBar.jsx
+++ src/app/(frontend)/[locale]/_components/Home/Header/SearchBar.jsx
@@ -4,7 +4,7 @@ import { VscSettings } from 'react-icons/vsc'
 
 const SearchBar = ({ t }) => {
   return (
-    <div className="w-full p-4 flex justify-between items-center rounded-lg bg-white shadow-lg hover:shadow-xl transition-shadow duration-300 -mt-5 z-10">
+    <div className="w-full p-4 flex justify-between items-center rounded-lg bg-white shadow-lg hover:shadow-xl transition-shadow duration-300 -mt-5 z-1">
       <div className="flex items-start gap-4">
         <CiSearch className="text-xl" />
 
